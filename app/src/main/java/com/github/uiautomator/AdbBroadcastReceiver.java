1117c4da46750eb094dff312883d30dfc1384e92
doorxp <doorxp@msn.com>
2025-10-22 16:32:17 +0800

    tools 8.13.0 add led control

--- app/src/main/java/com/github/uiautomator/AdbBroadcastReceiver.java
+++ app/src/main/java/com/github/uiautomator/AdbBroadcastReceiver.java
@@ -4,7 +4,12 @@ package com.github.uiautomator;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
 import android.location.LocationManager;
+import android.os.Build;
 import android.util.Log;
 
 public class AdbBroadcastReceiver extends BroadcastReceiver {
@@ -22,7 +27,52 @@ public class AdbBroadcastReceiver extends BroadcastReceiver {
             if (mockWifi != null) {
                 mockWifi.shutdown();
             }
-        } else {
+        }
+        else if(intent.getAction().equals("led.on")) {
+            if(!context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
+                return;
+            }
+            CameraManager cameraManager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
+            try {
+                String[] cameraIds = cameraManager.getCameraIdList();
+                for (String cameraId : cameraIds) {
+                    CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraId);
+                    Boolean flashAvailable = characteristics.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+
+                    // 只对有闪光灯的相机设置手电筒模式
+                    if (flashAvailable != null && flashAvailable) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            cameraManager.setTorchMode(cameraId, true);
+                        }
+                    }
+                }
+            } catch (CameraAccessException e) {
+                Log.e("AdbIME", "Failed to access camera", e);
+            }
+        }
+        else if(intent.getAction().equals("led.off")) {
+            if(!context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
+                return;
+            }
+            CameraManager cameraManager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
+            try {
+                String[] cameraIds = cameraManager.getCameraIdList();
+                for (String cameraId : cameraIds) {
+                    CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraId);
+                    Boolean flashAvailable = characteristics.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+
+                    // 只对有闪光灯的相机设置手电筒模式
+                    if (flashAvailable != null && flashAvailable) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            cameraManager.setTorchMode(cameraId, false);
+                        }
+                    }
+                }
+            } catch (CameraAccessException e) {
+                Log.e("AdbIME", "Failed to access camera", e);
+            }
+        }
+        else {
             mockGPS = new MockLocationProvider(LocationManager.GPS_PROVIDER, context);
             mockWifi = new MockLocationProvider(LocationManager.NETWORK_PROVIDER, context);
 
